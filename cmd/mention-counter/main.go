7c04d440167703a2cf2f8e83b95dd0ea8581d8d8
Eric Volpert <ericvolp12@gmail.com>
2023-04-22 10:39:55 -0700

    Fix gitignore

---
+++ cmd/mention-counter/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"os"
+	"sort"
+	"time"
+
+	"github.com/bluesky-social/indigo/events"
+	intEvents "github.com/ericvolp12/bsky-experiments/pkg/events"
+	"github.com/gorilla/websocket"
+)
+
+// MentionCount is used for sorting and storing mention counts
+type MentionCount struct {
+	Handle string
+	Count  int
+}
+
+func main() {
+	ctx := context.Background()
+	// Replace with the WebSocket URL you want to connect to.
+	u := url.URL{Scheme: "wss", Host: "bsky.social", Path: "/xrpc/com.atproto.sync.subscribeRepos"}
+
+	bsky, err := intEvents.NewBSky()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	ticker := time.NewTicker(30 * time.Second)
+	quit := make(chan struct{})
+
+	// Run a routine that dumps mention counts to a file every 30 seconds
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				fmt.Printf("\u001b[90m[%s]\u001b[32m writing mention counts to file...\u001b[0m\n", time.Now().Format("02.01.06 15:04:05"))
+				mentionFile := os.Getenv("MENTIONS_FILE")
+				if mentionFile == "" {
+					mentionFile = "mention-counts.txt"
+				}
+				mentionFileContents := ""
+				// Sort mentions by count, descending
+				sortedCounts := []MentionCount{}
+				for k, v := range bsky.MentionCounters {
+					sortedCounts = append(sortedCounts, MentionCount{k, v})
+				}
+
+				sort.Slice(sortedCounts, func(i, j int) bool {
+					return sortedCounts[i].Count > sortedCounts[j].Count
+				})
+
+				for _, count := range sortedCounts {
+					mentionFileContents += fmt.Sprintf("%s: %d\n", count.Handle, count.Count)
+				}
+
+				err := ioutil.WriteFile(mentionFile, []byte(mentionFileContents), 0644)
+				if err != nil {
+					fmt.Printf("error writing mention counts to file: %s", err)
+				}
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+
+	events.HandleRepoStream(ctx, c, &events.RepoStreamCallbacks{
+		RepoCommit: bsky.HandleRepoCommit,
+		RepoInfo:   intEvents.HandleRepoInfo,
+		Error:      intEvents.HandleError,
+	})
+}
