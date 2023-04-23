e0968f5d0046018cf9d120ca7975d63e0581566a
Eric Volpert <ericvolp12@gmail.com>
2023-04-23 16:46:40 -0700

    Get users working better with better progress updates

--- cmd/get-users/main.go
+++ cmd/get-users/main.go
@@ -11,38 +11,28 @@ import (
 	comatproto "github.com/bluesky-social/indigo/api/atproto"
 	"github.com/bluesky-social/indigo/api/bsky"
 	"github.com/bluesky-social/indigo/xrpc"
-	intXRPC "github.com/ericvolp12/bsky-experiments/pkg/xrpc"
 
 	"github.com/cheggaaa/pb/v3"
+	intXRPC "github.com/ericvolp12/bsky-experiments/pkg/xrpc"
 	"golang.org/x/time/rate"
 )
 
-func getProfiles(ctx context.Context, client *xrpc.Client, dids []string, limiter *rate.Limiter, progress *pb.ProgressBar) ([]string, error) {
+func getProfile(ctx context.Context, client *xrpc.Client, did string, limiter *rate.Limiter, progress *pb.ProgressBar) (string, error) {
 	// Use rate limiter before each request
 	err := limiter.Wait(ctx)
 	if err != nil {
-		return nil, fmt.Errorf("error waiting for rate limiter: %w", err)
+		return "", fmt.Errorf("error waiting for rate limiter: %w", err)
 	}
 
-	outProfiles, err := bsky.ActorGetProfiles(ctx, client, dids)
+	profile, err := bsky.ActorGetProfile(ctx, client, did)
 	if err != nil {
-		return nil, fmt.Errorf("error getting profile for %+v: %w", dids, err)
-	}
-
-	if outProfiles == nil {
-		return nil, fmt.Errorf("nil profiles for %+v", dids)
-	}
-
-	handles := []string{}
-
-	for _, profile := range outProfiles.Profiles {
-		handles = append(handles, profile.Handle)
+		return "", fmt.Errorf("error getting profile for %s: %w", did, err)
 	}
 
 	// Increment the progress bar
-	progress.Add(len(dids))
+	progress.Increment()
 
-	return handles, nil
+	return profile.Handle, nil
 }
 
 func main() {
@@ -53,7 +43,7 @@ func main() {
 	}
 
 	// Set up a rate limiter to limit requests to 50 per second
-	limiter := rate.NewLimiter(rate.Limit(50), 1)
+	limiter := rate.NewLimiter(rate.Limit(50), 5)
 
 	cursor := ""
 
@@ -65,54 +55,59 @@ func main() {
 	progress.SetMaxWidth(80)
 	progress.Start()
 
-	// Update the progress bar total count
-	progress.SetTotal(35000)
+	dids := []string{}
 
 	for {
+		// Use rate limiter before each request
+		err := limiter.Wait(ctx)
+		if err != nil {
+			fmt.Printf("error waiting for rate limiter: %v", err)
+			return
+		}
+
 		repoOutput, err := comatproto.SyncListRepos(ctx, client, cursor, 1000)
 		if err != nil {
 			fmt.Printf("error listing repos: %s\n", err)
 		}
 
-		// Create a wait group and a channel to receive handles
-		var wg sync.WaitGroup
-		handleChan := make(chan []string, len(repoOutput.Repos)/25+1)
-
-		for i := 0; i < len(repoOutput.Repos); i += 25 {
-			wg.Add(1)
-			end := i + 25
-			if end > len(repoOutput.Repos) {
-				end = len(repoOutput.Repos)
-			}
-
-			dids := make([]string, end-i)
-			for j := i; j < end; j++ {
-				dids[j-i] = repoOutput.Repos[j].Did
-			}
-
-			go func(dids []string) {
-				defer wg.Done()
-				batchHandles, err := getProfiles(ctx, client, dids, limiter, progress)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				handleChan <- batchHandles
-			}(dids)
-		}
-
-		// Wait for all goroutines to finish
-		wg.Wait()
-		close(handleChan)
-
-		// Collect handles from the channel
-		for handleSlice := range handleChan {
-			handles = append(handles, handleSlice...)
+		for _, repo := range repoOutput.Repos {
+			dids = append(dids, repo.Did)
 		}
 
 		if repoOutput.Cursor == nil {
 			break
 		}
+
+		cursor = *repoOutput.Cursor
+
+		progress.AddTotal(int64(len(repoOutput.Repos)))
+	}
+
+	// Create a wait group and a channel to receive handles
+	var wg sync.WaitGroup
+	handleChan := make(chan string, len(dids))
+
+	for _, did := range dids {
+		wg.Add(1)
+
+		go func(did string) {
+			defer wg.Done()
+			handle, err := getProfile(ctx, client, did, limiter, progress)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			handleChan <- handle
+		}(did)
+	}
+
+	// Wait for all goroutines to finish
+	wg.Wait()
+	close(handleChan)
+
+	// Collect handles from the channel
+	for handle := range handleChan {
+		handles = append(handles, handle)
 	}
 
 	// Finish the progress bar
