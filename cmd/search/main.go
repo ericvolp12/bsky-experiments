c5761ed83a6dc4c0a2bb8ad562a44e00385ebbf3
Jaz <ericvolp12@gmail.com>
2023-05-06 01:37:20 -0700

    Harden endpoint, add caches, add metrics

--- cmd/search/main.go
+++ cmd/search/main.go
@@ -6,11 +6,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 
 	"github.com/ericvolp12/bsky-experiments/pkg/search"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	lru "github.com/hashicorp/golang-lru"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	ginprometheus "github.com/zsais/go-gin-prometheus"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
 	"go.opentelemetry.io/otel"
@@ -21,6 +26,28 @@ import (
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
+
+// Initialize Prometheus Metrics for cache hits and misses
+var cacheHits = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "bsky_cache_hits_total",
+	Help: "The total number of cache hits",
+}, []string{"cache_type"})
+
+var cacheMisses = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "bsky_cache_misses_total",
+	Help: "The total number of cache misses",
+}, []string{"cache_type"})
+
+type ThreadViewCacheEntry struct {
+	ThreadView []search.PostView
+	Expiration time.Time
+}
+
+type API struct {
+	PostRegistry       *search.PostRegistry
+	ThreadViewCacheTTL time.Duration
+	ThreadViewCache    *lru.ARCCache
+}
 
 func main() {
 	ctx := context.Background()
@@ -49,12 +76,38 @@ func main() {
 	}
 	defer postRegistry.Close()
 
+	// Hellthread is around 160KB right now so 1000 worst-case threads should be around 160MB
+	threadViewCache, err := lru.NewARC(1000)
+	if err != nil {
+		log.Fatalf("Failed to create threadViewCache: %v", err)
+	}
+
+	api := &API{
+		PostRegistry:       postRegistry,
+		ThreadViewCache:    threadViewCache,
+		ThreadViewCacheTTL: 5 * time.Minute,
+	}
+
 	router := gin.Default()
 
 	router.Use(otelgin.Middleware("BSkySearchAPI"))
 
 	// CORS middleware
-	router.Use(cors.Default())
+	router.Use(cors.New(
+		cors.Config{
+			AllowOrigins: []string{"https://bsky.jazco.dev"},
+			AllowMethods: []string{"GET", "OPTIONS"},
+			AllowHeaders: []string{"Origin", "Content-Length", "Content-Type"},
+			AllowOriginFunc: func(origin string) bool {
+				u, err := url.Parse(origin)
+				if err != nil {
+					return false
+				}
+				// Allow localhost and localnet requests for localdev
+				return u.Hostname() == "localhost" || u.Hostname() == "10.0.6.32"
+			},
+		},
+	))
 
 	// Prometheus middleware
 	p := ginprometheus.NewPrometheus("gin")
@@ -64,8 +117,7 @@ func main() {
 		authorID := c.Query("authorID")
 		authorHandle := c.Query("authorHandle")
 		postID := c.Query("postID")
-
-		processThreadRequest(c, postRegistry, authorID, authorHandle, postID)
+		api.processThreadRequest(c, authorID, authorHandle, postID)
 	})
 
 	port := os.Getenv("PORT")
@@ -77,7 +129,7 @@ func main() {
 	router.Run(fmt.Sprintf(":%s", port))
 }
 
-func processThreadRequest(c *gin.Context, postRegistry *search.PostRegistry, authorID, authorHandle, postID string) {
+func (api *API) processThreadRequest(c *gin.Context, authorID, authorHandle, postID string) {
 	ctx := c.Request.Context()
 	tracer := otel.Tracer("search-api")
 	ctx, span := tracer.Start(ctx, "processThreadRequest")
@@ -99,7 +151,7 @@ func processThreadRequest(c *gin.Context, postRegistry *search.PostRegistry, aut
 	}
 
 	if authorID == "" {
-		authors, err := postRegistry.GetAuthorsByHandle(ctx, authorHandle)
+		authors, err := api.PostRegistry.GetAuthorsByHandle(ctx, authorHandle)
 		if err != nil {
 			log.Printf("Error getting authors: %v", err)
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -115,9 +167,33 @@ func processThreadRequest(c *gin.Context, postRegistry *search.PostRegistry, aut
 	}
 
 	// Get highest level post in thread
-	rootPost, err := getRootOrOldestParent(ctx, postRegistry, postID)
+	rootPost, err := api.getRootOrOldestParent(ctx, postID)
+	if err != nil {
+		if errors.As(err, &search.NotFoundError{}) {
+			log.Printf("Post with postID '%s' not found", postID)
+			c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Post with postID '%s' not found", postID)})
+		} else {
+			log.Printf("Error getting root post: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	}
 
-	threadView, err := postRegistry.GetThreadView(ctx, rootPost.ID, rootPost.AuthorDID)
+	// Check for the thread in the ARC Cache
+	entry, ok := api.ThreadViewCache.Get(postID)
+	if ok {
+		cacheEntry := entry.(ThreadViewCacheEntry)
+		if cacheEntry.Expiration.After(time.Now()) {
+			cacheHits.WithLabelValues("thread").Inc()
+			span.SetAttributes(attribute.Bool("thread_cache_hit", true))
+			c.JSON(http.StatusOK, cacheEntry.ThreadView)
+			return
+		}
+		// If the thread is expired, remove it from the cache
+		api.ThreadViewCache.Remove(postID)
+	}
+
+	threadView, err := api.PostRegistry.GetThreadView(ctx, rootPost.ID, rootPost.AuthorDID)
 	if err != nil {
 		if errors.As(err, &search.NotFoundError{}) {
 			log.Printf("Thread with authorID '%s' and postID '%s' not found", authorID, postID)
@@ -129,16 +205,24 @@ func processThreadRequest(c *gin.Context, postRegistry *search.PostRegistry, aut
 		return
 	}
 
+	span.SetAttributes(attribute.Bool("thread_cache_hit", false))
+
+	// Update the ARC Cache
+	api.ThreadViewCache.Add(postID, ThreadViewCacheEntry{
+		ThreadView: threadView,
+		Expiration: time.Now().Add(api.ThreadViewCacheTTL),
+	})
+
 	c.JSON(http.StatusOK, threadView)
 }
 
-func getRootOrOldestParent(ctx context.Context, postRegistry *search.PostRegistry, postID string) (*search.Post, error) {
+func (api *API) getRootOrOldestParent(ctx context.Context, postID string) (*search.Post, error) {
 	tracer := otel.Tracer("search-api")
 	ctx, span := tracer.Start(ctx, "getRootOrOldestParent")
 	defer span.End()
 	// Get post from registry to look for root post
 	span.AddEvent("getRootOrOldestParent:ResolvePrimaryPost")
-	post, err := postRegistry.GetPost(ctx, postID)
+	post, err := api.PostRegistry.GetPost(ctx, postID)
 	if err != nil {
 		if errors.As(err, &search.NotFoundError{}) {
 			span.SetAttributes(attribute.Bool("primary_post_found", false))
@@ -152,7 +236,7 @@ func getRootOrOldestParent(ctx context.Context, postRegistry *search.PostRegistr
 	// If post has a root post and we've stored it, return it
 	if post.RootPostID != nil {
 		span.AddEvent("getRootOrOldestParent:ResolveRootPost")
-		rootPost, err := postRegistry.GetPost(ctx, *post.RootPostID)
+		rootPost, err := api.PostRegistry.GetPost(ctx, *post.RootPostID)
 		if err != nil {
 			// If we don't have the root post, continue to just return the oldest parent
 			if !errors.As(err, &search.NotFoundError{}) {
@@ -169,7 +253,7 @@ func getRootOrOldestParent(ctx context.Context, postRegistry *search.PostRegistr
 
 	// Otherwise, get the oldest parent from the registry
 	span.AddEvent("getRootOrOldestParent:ResolveOldestParent")
-	oldestParent, err := postRegistry.GetOldestPresentParent(ctx, postID)
+	oldestParent, err := api.PostRegistry.GetOldestPresentParent(ctx, postID)
 	if err != nil {
 		if errors.As(err, &search.NotFoundError{}) {
 			span.SetAttributes(attribute.Bool("oldest_parent_found", false))
