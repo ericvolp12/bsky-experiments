bcd0fdb3e6fc8d28a29168d4f4f3fd71521bdf69
Jaz <ericvolp12@gmail.com>
2023-05-10 20:32:35 -0700

    Tracing and proper logging for sentiment service

--- python/sentiment/bsky-sentiment/app.py
+++ python/sentiment/bsky-sentiment/app.py
@@ -1,11 +1,14 @@
 import logging
+import os
 from time import time
 
 from fastapi import FastAPI, Request
 from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from prometheus_fastapi_instrumentator import Instrumentator
 from pythonjsonlogger import jsonlogger
 from starlette.middleware.base import BaseHTTPMiddleware
@@ -13,43 +16,66 @@ from starlette.middleware.base import BaseHTTPMiddleware
 from .models import Decision, Post, PostList
 from .sentiment import get_sentiment
 
+# Set up JSON logging
+formatter = jsonlogger.JsonFormatter()
+handler = logging.StreamHandler()
+
+# Use OUR `formatter` to format all `logging` entries.
+handler.setFormatter(formatter)
+root_logger = logging.getLogger()
+root_logger.addHandler(handler)
+root_logger.setLevel(logging.INFO)
+
+for _log in ["uvicorn", "uvicorn.error"]:
+    # Clear the log handlers for uvicorn loggers, and enable propagation
+    # so the messages are caught by our root logger and formatted correctly
+    # by structlog
+    logging.getLogger(_log).handlers.clear()
+    logging.getLogger(_log).propagate = True
+
+# Since we re-create the access logs ourselves, to add all information
+# in the structured log, we clear the handlers and prevent the logs to propagate to
+# a logger higher up in the hierarchy (effectively rendering them silent).
+logging.getLogger("uvicorn.access").handlers.clear()
+logging.getLogger("uvicorn.access").propagate = False
+
 
 class LoggingMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         start_time = time()
         response = await call_next(request)
-        process_time = (time() - start_time) * 1000
-        log.info(
+        process_time = time() - start_time
+        logging.info(
             {
+                "message": "request handled",
                 "path": request.url.path,
                 "method": request.method,
                 "processing_time": process_time,
                 "status_code": response.status_code,
-            }
+                "query_params": request.query_params,
+            },
         )
         return response
 
 
-# Set up JSON logging
-log = logging.getLogger("uvicorn.error")
-if log.hasHandlers():
-    for handler in log.handlers:
-        log.removeHandler(handler)
-logHandler = logging.StreamHandler()
-formatter = jsonlogger.JsonFormatter()
-logHandler.setFormatter(formatter)
-log.addHandler(logHandler)
-
 # Set up OpenTelemetry
-# trace.set_tracer_provider(TracerProvider())
-# trace.get_tracer_provider().add_span_processor(
-#     SimpleSpanProcessor(ConsoleSpanExporter())
-# )
+otel_endpoint = os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
+if otel_endpoint:
+    resource = Resource(attributes={SERVICE_NAME: "bsky-sentiment"})
+    trace.set_tracer_provider(TracerProvider(resource=resource))
+    trace.get_tracer_provider().add_span_processor(
+        BatchSpanProcessor(
+            OTLPSpanExporter(
+                endpoint=otel_endpoint + "v1/traces",
+            )
+        )
+    )
 
 app = FastAPI()
 
 # Instrument FastAPI for OpenTelemetry
-# FastAPIInstrumentor.instrument_app(app)
+if otel_endpoint:
+    FastAPIInstrumentor.instrument_app(app)
 
 # Instrument FastAPI for Prometheus
 Instrumentator().instrument(app).expose(app, include_in_schema=False)
