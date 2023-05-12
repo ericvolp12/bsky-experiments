8c2054991bfa4bed678484224f01b5b3cad9666d
Jaz <ericvolp12@gmail.com>
2023-05-11 23:10:23 -0700

    Fix input tensor size limit

--- python/sentiment-batch/bsky-sentiment-batch/batch.py
+++ python/sentiment-batch/bsky-sentiment-batch/batch.py
@@ -86,7 +86,7 @@ def get_sentiment(post_text):
     logger.debug(f"Min Input ID: {input_ids.min()}")
 
     # Check if input shape is valid
-    if input_ids.shape[0] > 1 or input_ids.shape[1] > 514:
+    if input_ids.shape[0] > 1 or input_ids.shape[1] > 512:
         logger.error(f"Invalid input shape: {input_ids.shape} for text: {text}")
         return "u", 0
 
--- python/sentiment/bsky-sentiment/sentiment.py
+++ python/sentiment/bsky-sentiment/sentiment.py
@@ -41,7 +41,7 @@ def get_sentiment(post_text):
         logger.debug(f"Min Input ID: {input_ids.min()}")
 
         # Check if input shape is valid
-        if input_ids.shape[0] > 1 or input_ids.shape[1] > 514:
+        if input_ids.shape[0] > 1 or input_ids.shape[1] > 512:
             logger.error(f"Invalid input shape: {input_ids.shape} for text: {text}")
             span.set_attribute("invalid_input_shape", True)
             span.set_attribute("post_text", post_text)
