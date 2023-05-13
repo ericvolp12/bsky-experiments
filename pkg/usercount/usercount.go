de583cb687ced9878ee134277767fe129aa9fa3f
Jaz <ericvolp12@gmail.com>
2023-05-13 00:55:00 -0700

    Attempt at parallelizing user fetch

--- pkg/usercount/usercount.go
+++ pkg/usercount/usercount.go
@@ -20,6 +20,7 @@ type UserCount struct {
 	ClientMux        *sync.RWMutex
 	CurrentUserCount int
 	RateLimiter      *rate.Limiter
+	Cursors          []string
 }
 
 func NewUserCount(ctx context.Context, client *xrpc.Client) *UserCount {
@@ -54,6 +55,7 @@ func NewUserCount(ctx context.Context, client *xrpc.Client) *UserCount {
 		Client:      client,
 		RateLimiter: limiter,
 		ClientMux:   clientMux,
+		Cursors:     []string{""},
 	}
 }
 
@@ -65,10 +67,59 @@ func (uc *UserCount) GetUserCount(ctx context.Context) (int, error) {
 	tracer := otel.Tracer("usercount")
 	ctx, span := tracer.Start(ctx, "GetUserCount")
 	defer span.End()
-	cursor := ""
 
 	numUsers := 0
 
+	countChannel := make(chan int, len(uc.Cursors)-1)
+	errorChannel := make(chan error, len(uc.Cursors)-1)
+
+	// Iterate over all but the final cursor in goroutines
+	for i, cursor := range uc.Cursors {
+		if i == len(uc.Cursors)-1 {
+			break
+		}
+		go func(cursor string) {
+			log.Printf("getting repos with cursor %s\n", cursor)
+
+			// Use rate limiter before each request
+			err := uc.RateLimiter.Wait(ctx)
+			if err != nil {
+				fmt.Printf("error waiting for rate limiter: %v", err)
+				errorChannel <- fmt.Errorf("error waiting for rate limiter: %w", err)
+				return
+			}
+
+			span.AddEvent("AcquireClientRLock")
+			uc.ClientMux.RLock()
+			span.AddEvent("ClientRLockAcquired")
+			repoOutput, err := comatproto.SyncListRepos(ctx, uc.Client, cursor, 1000)
+			if err != nil {
+				fmt.Printf("error listing repos: %s\n", err)
+				errorChannel <- fmt.Errorf("error listing repos: %w", err)
+				return
+			}
+			span.AddEvent("ReleaseClientRLock")
+			uc.ClientMux.RUnlock()
+
+			countChannel <- len(repoOutput.Repos)
+		}(cursor)
+	}
+
+	// Read from the channels
+	for i := 0; i < len(uc.Cursors)-1; i++ {
+		select {
+		case count := <-countChannel:
+			numUsers += count
+		case err := <-errorChannel:
+			return -1, err
+		}
+	}
+
+	log.Printf("fetching final cursor: %s\n", uc.Cursors[len(uc.Cursors)-1])
+
+	cursor := uc.Cursors[len(uc.Cursors)-1]
+	// Iterate over the final cursor in the main thread
+	// this also builds up the cursor list on the first run
 	for {
 		// Use rate limiter before each request
 		err := uc.RateLimiter.Wait(ctx)
@@ -95,6 +146,7 @@ func (uc *UserCount) GetUserCount(ctx context.Context) (int, error) {
 		}
 
 		cursor = *repoOutput.Cursor
+		uc.Cursors = append(uc.Cursors, cursor)
 	}
 
 	uc.CurrentUserCount = numUsers
