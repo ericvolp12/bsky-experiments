6e8729299de0c4e211084a8990ce3d2405c2e746
Jaz <ericvolp12@gmail.com>
2023-05-19 20:10:22 -0700

    Add more latency buckets

--- python/object-detection/bsky-object-detection/app.py
+++ python/object-detection/bsky-object-detection/app.py
@@ -85,7 +85,7 @@ if otel_endpoint:
 # Instrument FastAPI for Prometheus
 Instrumentator().instrument(
     app,
-    latency_lowr_buckets=[0.01, 0.05, 0.1, 0.2, 0.5, 1.0, 2.5, 5, 10],
+    latency_lowr_buckets=[0.01, 0.05, 0.1, 0.2, 0.5, 1.0, 2.5, 5, 10, 20, 30, 60],
 ).expose(app, include_in_schema=False)
 
 # Add logging middleware
