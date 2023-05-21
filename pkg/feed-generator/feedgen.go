29b0411302895ed0b2378d2f853448ebd3b77d98
Jaz <ericvolp12@gmail.com>
2023-05-21 12:26:06 -0700

    Add more tracing to feed generation

--- pkg/feed-generator/feedgen.go
+++ pkg/feed-generator/feedgen.go
@@ -16,6 +16,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 )
 
 var feedRequestCounter = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -97,9 +99,14 @@ func (fg *FeedGenerator) GetWellKnownDID(c *gin.Context) {
 }
 
 func (fg *FeedGenerator) DescribeFeedGenerator(c *gin.Context) {
+	tracer := otel.Tracer("feed-generator")
+	ctx, span := tracer.Start(c.Request.Context(), "FeedGenerator:DescribeFeedGenerator")
+	defer span.End()
+
 	clusterAliases := []string{}
-	clusters, err := fg.PostRegistry.GetClusters(c.Request.Context())
+	clusters, err := fg.PostRegistry.GetClusters(ctx)
 	if err != nil {
+		span.SetAttributes(attribute.String("clusters.error", err.Error()))
 		log.Printf("failed to get clusters: %s", err.Error())
 	} else {
 		for _, cluster := range clusters {
@@ -107,13 +114,18 @@ func (fg *FeedGenerator) DescribeFeedGenerator(c *gin.Context) {
 		}
 	}
 
+	span.SetAttributes(attribute.Int("clusters.length", len(clusterAliases)))
+
 	labels := []string{}
-	uniqueLabels, err := fg.PostRegistry.GetUniqueLabels(c.Request.Context())
+	uniqueLabels, err := fg.PostRegistry.GetUniqueLabels(ctx)
 	if err != nil {
+		span.SetAttributes(attribute.String("labels.error", err.Error()))
 		log.Printf("failed to get unique labels: %s", err.Error())
 	} else {
 		labels = append(labels, uniqueLabels...)
 	}
+
+	span.SetAttributes(attribute.Int("labels.length", len(labels)))
 
 	feedPrefix := "at://did:web:feedsky.jazco.io/app.bsky.feed.generator/"
 
@@ -135,13 +147,23 @@ func (fg *FeedGenerator) DescribeFeedGenerator(c *gin.Context) {
 		Feeds: feedDescriptions,
 	}
 
+	span.SetAttributes(attribute.Int("feeds.length", len(feedDescriptions)))
+
 	c.JSON(http.StatusOK, feedGeneratorDescription)
 }
 
 func (fg *FeedGenerator) UpdateClusterAssignments(c *gin.Context) {
+	tracer := otel.Tracer("feed-generator")
+	ctx, span := tracer.Start(c.Request.Context(), "FeedGenerator:UpdateClusterAssignments")
+	defer span.End()
+
 	log.Println("Updating cluster assignments...")
 	// Iterate over all authors in the Manager and update them in the registry
 	errs := make([]error, 0)
+	errStrings := make([]string, 0)
+
+	span.SetAttributes(attribute.Int("authors.length", len(fg.ClusterManager.DIDClusterMap)))
+	span.SetAttributes(attribute.Int("clusters.length", len(fg.ClusterManager.Clusters)))
 
 	count := 0
 	for _, author := range fg.ClusterManager.DIDClusterMap {
@@ -158,16 +180,20 @@ func (fg *FeedGenerator) UpdateClusterAssignments(c *gin.Context) {
 			log.Println(newErr.Error())
 			continue
 		}
-		err = fg.PostRegistry.AssignAuthorToCluster(c.Request.Context(), author.UserDID, int32(clusterID))
+		err = fg.PostRegistry.AssignAuthorToCluster(ctx, author.UserDID, int32(clusterID))
 		if err != nil {
 			newErr := fmt.Errorf("failed to assign author %s to cluster %d: %w", author.UserDID, clusterID, err)
 			errs = append(errs, newErr)
+			errStrings = append(errStrings, newErr.Error())
 			log.Println(newErr.Error())
 			continue
 		}
 	}
 
 	log.Println("Finished updating cluster assignments")
+
+	span.SetAttributes(attribute.Int("errors.length", len(errs)))
+	span.SetAttributes(attribute.StringSlice("errors", errStrings))
 
 	c.JSON(http.StatusOK, gin.H{"message": "cluster assignments updated", "errors": errs})
 }
@@ -176,12 +202,18 @@ func (fg *FeedGenerator) GetFeedSkeleton(c *gin.Context) {
 	// Incoming requests should have a query parameter "feed" that looks like: at://did:web:feedsky.jazco.io/app.bsky.feed.generator/feed-name
 	// Also a query parameter "limit" that looks like: 50
 	// Also a query parameter "cursor" that contains the last post ID from the previous page of results
+	tracer := otel.Tracer("feed-generator")
+	ctx, span := tracer.Start(c.Request.Context(), "FeedGenerator:GetFeedSkeleton")
+	defer span.End()
+
 	start := time.Now()
 	feedQuery := c.Query("feed")
 	if feedQuery == "" {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "feed query parameter is required"})
 		return
 	}
+
+	span.SetAttributes(attribute.String("feed.query", feedQuery))
 
 	// Make sure the feed query is for our feed generator service
 	if !strings.HasPrefix(feedQuery, "at://did:web:feedsky.jazco.io/app.bsky.feed.generator/") {
@@ -196,10 +228,14 @@ func (fg *FeedGenerator) GetFeedSkeleton(c *gin.Context) {
 		return
 	}
 
+	span.SetAttributes(attribute.String("feed.name.raw", feedName))
+
 	// Check if this is a legacy feed name
 	if fg.LegacyFeedNames[feedName] != "" {
 		feedName = fg.LegacyFeedNames[feedName]
 	}
+
+	span.SetAttributes(attribute.String("feed.name.parsed", feedName))
 
 	var cluster *string
 
@@ -207,6 +243,7 @@ func (fg *FeedGenerator) GetFeedSkeleton(c *gin.Context) {
 	if strings.HasPrefix(feedName, "cluster-") {
 		clusterAlias := strings.TrimPrefix(feedName, "cluster-")
 		cluster = &clusterAlias
+		span.SetAttributes(attribute.String("feed.cluster", clusterAlias))
 	}
 
 	feedRequestCounter.WithLabelValues(feedName).Inc()
@@ -214,47 +251,61 @@ func (fg *FeedGenerator) GetFeedSkeleton(c *gin.Context) {
 	// Get the limit from the query, default to 50, maximum of 250
 	limit := int32(50)
 	limitQuery := c.Query("limit")
+	span.SetAttributes(attribute.String("feed.limit.raw", limitQuery))
 	if limitQuery != "" {
 		parsedLimit, err := strconv.ParseInt(limitQuery, 10, 32)
 		if err != nil {
+			span.SetAttributes(attribute.Bool("feed.limit.failed_to_parse", true))
 			limit = 50
 		} else {
 			limit = int32(parsedLimit)
 			if limit > 250 {
+				span.SetAttributes(attribute.Bool("feed.limit.clamped", true))
 				limit = 250
 			}
 		}
 	}
 
+	span.SetAttributes(attribute.Int64("feed.limit.parsed", int64(limit)))
+
 	// Get the cursor from the query
 	cursor := c.Query("cursor")
+
+	span.SetAttributes(attribute.String("feed.cursor.raw", cursor))
 
 	var posts []*search.Post
 
 	// Get cluster posts if a cluster is specified
 	if cluster != nil {
-		postsFromRegistry, err := fg.PostRegistry.GetPostsPageForCluster(c.Request.Context(), *cluster, fg.DefaultLookback, limit, cursor)
+		postsFromRegistry, err := fg.PostRegistry.GetPostsPageForCluster(ctx, *cluster, fg.DefaultLookback, limit, cursor)
 		if err != nil {
 			if errors.As(err, &search.NotFoundError{}) {
+				span.SetAttributes(attribute.Bool("feed.cluster.not_found", true))
 				c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
 				return
 			}
+			span.SetAttributes(attribute.Bool("feed.cluster.error", true))
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
 
 		posts = postsFromRegistry
 	} else { // Otherwise lookup labels
-		postsFromRegistry, err := fg.PostRegistry.GetPostsPageForLabel(c.Request.Context(), feedName, fg.DefaultLookback, limit, cursor)
+		postsFromRegistry, err := fg.PostRegistry.GetPostsPageForLabel(ctx, feedName, fg.DefaultLookback, limit, cursor)
 		if err != nil {
 			if errors.As(err, &search.NotFoundError{}) {
+				span.SetAttributes(attribute.Bool("feed.label.not_found", true))
 				c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
 				return
 			}
-
+			span.SetAttributes(attribute.Bool("feed.label.error", true))
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 		posts = postsFromRegistry
 	}
+
+	span.SetAttributes(attribute.Int("feed.posts_returned", len(posts)))
 
 	feedItems := make([]FeedPostItem, len(posts))
 	for i, post := range posts {
