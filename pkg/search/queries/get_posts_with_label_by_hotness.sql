cbc404298b12eb4b55243f6c1a9f6d2159a7fe6d
Jaz <ericvolp12@gmail.com>
2023-05-26 18:01:39 -0700

    Hotness falloff increase

--- pkg/search/queries/get_posts_with_label_by_hotness.sql
+++ pkg/search/queries/get_posts_with_label_by_hotness.sql
@@ -1,7 +1,8 @@
 -- name: GetPostsPageWithLabelSortedByHotness :many
 SELECT p.id, p.text, p.parent_post_id, p.root_post_id, p.author_did, p.created_at, 
        p.has_embedded_media, p.parent_relationship, p.sentiment, p.sentiment_confidence,
-       (COALESCE(pl.like_count, 0) / EXTRACT(EPOCH FROM NOW() - p.created_at) / 60)::FLOAT AS hotness
+       (COALESCE(pl.like_count, 0) / GREATEST(1, EXTRACT(EPOCH FROM NOW() - p.created_at) / 60) *
+        EXP(GREATEST(0, (EXTRACT(EPOCH FROM NOW() - p.created_at) / 60 - 360) / 360)))::FLOAT AS hotness
 FROM posts p
 JOIN post_labels ON p.id = post_labels.post_id
 LEFT JOIN post_likes pl ON p.id = pl.post_id 
