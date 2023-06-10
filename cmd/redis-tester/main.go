3ec1ba9513660f9e1d909c2618852ee1a4ebcf6d
Jaz <ericvolp12@gmail.com>
2023-06-09 22:10:33 -0700

    Redis tester initial commit

---
+++ cmd/redis-tester/main.go
@@ -0,0 +1,306 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/namesgenerator"
+	"github.com/redis/go-redis/v9"
+)
+
+type TestParam struct {
+	Inserts      int
+	ValueSize    int
+	ReadAmp      int
+	PipelineSize int
+	TestName     string
+	RedisBackend string
+	Repetitions  int
+}
+
+var backendImageMap = map[string]string{
+	"redis-stack": "redis/redis-stack-server:latest",
+	"dragonfly":   "docker.dragonflydb.io/dragonflydb/dragonfly:latest",
+}
+
+func main() {
+	params := []TestParam{
+		{Inserts: 100_000, ValueSize: 5, ReadAmp: 50, PipelineSize: -1, TestName: "No-Pipeline", RedisBackend: "redis-stack", Repetitions: 3},
+		{Inserts: 100_000, ValueSize: 1_000, ReadAmp: 50, PipelineSize: -1, TestName: "No-Pipeline", RedisBackend: "redis-stack", Repetitions: 3},
+		{Inserts: 100_000, ValueSize: 10_000, ReadAmp: 50, PipelineSize: -1, TestName: "No-Pipeline", RedisBackend: "redis-stack", Repetitions: 3},
+
+		{Inserts: 100_000, ValueSize: 5, ReadAmp: 50, PipelineSize: -1, TestName: "No-Pipeline", RedisBackend: "dragonfly", Repetitions: 3},
+		{Inserts: 100_000, ValueSize: 1_000, ReadAmp: 50, PipelineSize: -1, TestName: "No-Pipeline", RedisBackend: "dragonfly", Repetitions: 3},
+		{Inserts: 100_000, ValueSize: 10_000, ReadAmp: 50, PipelineSize: -1, TestName: "No-Pipeline", RedisBackend: "dragonfly", Repetitions: 3},
+
+		{Inserts: 500_000, ValueSize: 5, ReadAmp: 50, PipelineSize: 10_000, TestName: "Pipeline", RedisBackend: "redis-stack", Repetitions: 3},
+		{Inserts: 500_000, ValueSize: 1_000, ReadAmp: 50, PipelineSize: 10_000, TestName: "Pipeline", RedisBackend: "redis-stack", Repetitions: 3},
+		{Inserts: 500_000, ValueSize: 1_000, ReadAmp: 50, PipelineSize: 1_000, TestName: "Pipeline", RedisBackend: "redis-stack", Repetitions: 3},
+		{Inserts: 100_000, ValueSize: 10_000, ReadAmp: 50, PipelineSize: 1_000, TestName: "Pipeline", RedisBackend: "redis-stack", Repetitions: 3},
+
+		{Inserts: 500_000, ValueSize: 5, ReadAmp: 50, PipelineSize: 10_000, TestName: "Pipeline", RedisBackend: "dragonfly", Repetitions: 3},
+		{Inserts: 500_000, ValueSize: 1_000, ReadAmp: 50, PipelineSize: 10_000, TestName: "Pipeline", RedisBackend: "dragonfly", Repetitions: 3},
+		{Inserts: 500_000, ValueSize: 1_000, ReadAmp: 50, PipelineSize: 1_000, TestName: "Pipeline", RedisBackend: "dragonfly", Repetitions: 3},
+		{Inserts: 100_000, ValueSize: 10_000, ReadAmp: 50, PipelineSize: 1_000, TestName: "Pipeline", RedisBackend: "dragonfly", Repetitions: 3},
+	}
+
+	runtimes := make([][]time.Duration, len(params))
+
+	for idx, param := range params {
+		runtimes = append(runtimes, runTest(param, idx))
+	}
+
+	// Average the runtimes for each test and print them out.
+	for idx, param := range params {
+		var total time.Duration
+		for _, runtime := range runtimes[idx] {
+			total += runtime
+		}
+		avg := total / time.Duration(param.Repetitions)
+		fmt.Printf("%s\t%s\t%d\t%d\t%d\t%d\t%.3f\t%d\n", param.TestName, param.RedisBackend, param.Inserts, param.ValueSize, param.ReadAmp, param.PipelineSize, avg.Seconds(), param.Repetitions)
+	}
+}
+
+func runTest(param TestParam, idx int) []time.Duration {
+	// Create a new Redis container for each test.
+	ctx := context.Background()
+	log.SetPrefix(fmt.Sprintf("[%s:%d] ", param.TestName, idx))
+
+	durations := make([]time.Duration, param.Repetitions)
+
+	for i := 0; i < param.Repetitions; i++ {
+
+		cli, err := client.NewEnvClient()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		image := backendImageMap[param.RedisBackend]
+		containerName := namesgenerator.GetRandomName(0)
+
+		resp, err := cli.ContainerCreate(ctx, &container.Config{
+			Image: image,
+		}, &container.HostConfig{
+			NetworkMode: "host",
+		}, nil, nil, containerName)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+			log.Fatal(err)
+		}
+
+		// Wait for Redis to start up.
+		time.Sleep(5 * time.Second)
+
+		// Test the connection to Redis.
+		conn := redis.NewClient(&redis.Options{
+			Addr: "localhost:6379",
+			DB:   0,
+		})
+
+		// Try to ping the Redis server 5 times before giving up.
+		redisReady := false
+		for i := 0; i < 5; i++ {
+			_, err := conn.Ping(ctx).Result()
+			if err == nil {
+				redisReady = true
+				break
+			}
+			time.Sleep(1 * time.Second)
+		}
+
+		if !redisReady {
+			log.Fatal("Failed to ping Redis server after 5 attempts")
+		}
+
+		start := time.Now()
+
+		// Run the test
+		if param.TestName == "No-Pipeline" {
+			err = TestRedisWithoutPipelines(param)
+		} else {
+			err = TestRedisWithPipelines(param)
+		}
+
+		durations = append(durations, time.Since(start))
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Cleanup: Stop and remove the Redis container.
+		if err := cli.ContainerStop(ctx, resp.ID, container.StopOptions{}); err != nil {
+			log.Fatal(err)
+		}
+		if err := cli.ContainerRemove(ctx, resp.ID, types.ContainerRemoveOptions{}); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return durations
+}
+
+func TestRedisWithPipelines(param TestParam) error {
+	ctx := context.Background()
+
+	n := param.Inserts
+	maxPipelineSize := param.PipelineSize
+
+	// Initialize Redis
+	conn := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0,
+	})
+
+	// Generate a bunch of keys
+	keys := make([]string, n)
+	for i := 0; i < n; i++ {
+		keys[i] = "key" + fmt.Sprint(i)
+	}
+
+	value := ""
+	for k := 0; k < param.ValueSize; k++ {
+		value += fmt.Sprint(rand.Intn(10))
+	}
+
+	// Add keys in batches
+	for i := 0; i < n; i += maxPipelineSize {
+		pipeline := conn.Pipeline()
+
+		for j := i; j < i+maxPipelineSize; j++ {
+			pipeline.Set(ctx, keys[j], value, time.Minute*2)
+		}
+
+		// Execute the pipeline
+		_, err := pipeline.Exec(ctx)
+		if err != nil {
+			return fmt.Errorf("pipeline.Exec: %v", err)
+		}
+	}
+
+	// Read back the keys in goroutines
+	numRoutines := param.ReadAmp
+	errChan := make(chan []error, numRoutines)
+
+	for i := 0; i < numRoutines; i++ {
+		go func() {
+			errs := []error{}
+
+			// Read in batches
+			for i := 0; i < n; i += maxPipelineSize {
+				pipeline := conn.Pipeline()
+
+				for j := i; j < i+maxPipelineSize; j++ {
+					pipeline.Get(ctx, keys[j])
+				}
+
+				// Execute the pipeline
+				cmder, err := pipeline.Exec(ctx)
+				if err != nil {
+					errs = append(errs, err)
+				}
+
+				// Check that we got the right values
+				for j := i; j < i+maxPipelineSize; j++ {
+					cmd := cmder[j-i]
+					if cmd.Err() != nil {
+						errs = append(errs, cmd.Err())
+					}
+					if cmd.(*redis.StringCmd).Val() != value {
+						errs = append(errs, fmt.Errorf("wrong value"))
+					}
+				}
+			}
+
+			errChan <- errs
+		}()
+	}
+
+	// Wait for all goroutines to finish
+	errs := []error{}
+	for i := 0; i < numRoutines; i++ {
+		errs = append(errs, <-errChan...)
+	}
+
+	// Check that there were no errors
+	if len(errs) > 0 {
+		return fmt.Errorf("errs: %v", errs)
+	}
+
+	return nil
+}
+
+func TestRedisWithoutPipelines(param TestParam) error {
+	ctx := context.Background()
+
+	n := param.Inserts
+
+	// Initialize Redis
+	conn := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0,
+	})
+
+	// Generate a bunch of keys
+	keys := make([]string, n)
+	for i := 0; i < n; i++ {
+		keys[i] = "key" + fmt.Sprint(i)
+	}
+
+	value := ""
+	for k := 0; k < param.ValueSize; k++ {
+		value += fmt.Sprint(rand.Intn(10))
+	}
+
+	// Add keys
+	for i := 0; i < n; i++ {
+		_, err := conn.Set(ctx, keys[i], value, time.Minute*2).Result()
+		if err != nil {
+			return fmt.Errorf("conn.Set: %v", err)
+		}
+	}
+
+	// Read back the keys in goroutines
+	numRoutines := param.ReadAmp
+	errChan := make(chan []error, numRoutines)
+
+	for i := 0; i < numRoutines; i++ {
+		go func() {
+			errs := []error{}
+
+			for i := 0; i < n; i++ {
+				val, err := conn.Get(ctx, keys[i]).Result()
+				if err != nil {
+					errs = append(errs, err)
+				}
+				if val != value {
+					errs = append(errs, fmt.Errorf("wrong value"))
+				}
+			}
+
+			errChan <- errs
+		}()
+	}
+
+	// Wait for all goroutines to finish
+	errs := []error{}
+	for i := 0; i < numRoutines; i++ {
+		errs = append(errs, <-errChan...)
+	}
+
+	// Check that there were no errors
+	if len(errs) > 0 {
+		return fmt.Errorf("errs: %v", errs)
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/bluesky-social/indigo v0.0.0-20230608214909-03c8d951013d
 	github.com/cheggaaa/pb/v3 v3.1.2
 	github.com/decred/dcrd/dcrec/secp256k1 v1.0.3
+	github.com/docker/docker v24.0.2+incompatible
+	github.com/docker/go-connections v0.4.0
 	github.com/ericvolp12/go-gin-prometheus v0.0.0-20221219081010-fc0e0436c283
 	github.com/ericvolp12/jwt-go-secp256k1 v0.0.1
 	github.com/gin-contrib/cors v1.4.0
@@ -38,6 +40,7 @@ require (
 )
 
 require (
+	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/VividCortex/ewma v1.2.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bits-and-blooms/bitset v1.8.0 // indirect
@@ -49,6 +52,8 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v2 v2.0.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/docker/distribution v2.8.2+incompatible // indirect
+	github.com/docker/go-units v0.5.0 // indirect
 	github.com/fatih/color v1.14.1 // indirect
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
@@ -100,14 +105,18 @@ require (
 	github.com/mattn/go-runewidth v0.0.12 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/moby/term v0.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/multiformats/go-base32 v0.1.0 // indirect
 	github.com/multiformats/go-base36 v0.2.0 // indirect
 	github.com/multiformats/go-multicodec v0.8.1 // indirect
 	github.com/multiformats/go-multihash v0.2.1 // indirect
 	github.com/multiformats/go-varint v0.0.7 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
@@ -134,14 +143,17 @@ require (
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230321023759-10a507213a29 // indirect
+	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.8.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
 	google.golang.org/grpc v1.55.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/gorm v1.24.7-0.20230306060331-85eaf9eeda11 // indirect
+	gotest.tools/v3 v3.4.0 // indirect
 	lukechampine.com/blake3 v1.1.7 // indirect
 )
