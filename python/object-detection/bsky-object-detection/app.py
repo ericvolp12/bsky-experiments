31afebf0383334e56917e1122e6d72d03b21c640
Jaz <ericvolp12@gmail.com>
2024-02-08 22:35:42 -0800

    More unpacking

--- python/object-detection/bsky-object-detection/app.py
+++ python/object-detection/bsky-object-detection/app.py
@@ -128,7 +128,9 @@ async def fetch_and_batch_images(
         )
         if streams and len(streams) > 0:
             messages = streams[0][1]
-            image_metas = [ImageMeta(**message) for _, message in messages]
+            image_metas = [
+                ImageMeta(**message["image_meta"]) for _, message in messages
+            ]
             yield image_metas
             last_id = messages[-1][0]
 
